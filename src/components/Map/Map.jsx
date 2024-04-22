ed87ddcfafe0e98cf55bbfce5f3a15567150736f
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2024-04-22 23:07:52 +0530

    App Structure, Header and Map component is completed

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import Rating from "@material-ui/lab";
+import useStyles from "./MapStyles";
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery(('min-width:600px'));
+
+    const coordinates = {
+        lat: 0,
+        lng: 0
+    }
     return(
-        <div>
-            <h1>Map</h1>
-        </div>
+       <div className = {classes.mapContainer}>
+        <GoogleMapReact
+            bootstrapURLKeys = {{key: 'AIzaSyCER-1jiXG4XNunDnrC9HtH4FxGuTo1VKI'}}
+            defaultCenter = {coordinates}
+            center = {coordinates}
+            defaultZoom= {14}
+            margin = {[50,50,50,50]}
+            options = {''}
+            onChange = {''}
+            onChildClick = {''}
+        >
+        </GoogleMapReact>
+        
+       </div>
     );
 }
 export  default Map; 
